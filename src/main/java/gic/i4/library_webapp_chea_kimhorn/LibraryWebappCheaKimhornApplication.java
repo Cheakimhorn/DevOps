23c74d386470f890d6b186166bbcf75ca33375cb
cheakimhorn <kimhornchea612@gmail.com>
2025-05-09 11:27:25 +0700

    add

---
+++ src/main/java/gic/i4/library_webapp_chea_kimhorn/LibraryWebappCheaKimhornApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_chea_kimhorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappCheaKimhornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappCheaKimhornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_chea_kimhorn/LibraryWebappCheaKimhornApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_chea_kimhorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappCheaKimhornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
